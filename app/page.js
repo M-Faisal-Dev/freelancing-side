5802995c88d274d6ec9f08e4a15f31ff8fe447ce
Muhammad Faisal <faisaldeveloperr@gmail.com>
2023-07-17 22:47:07 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-extrabold">I am Stating Work</h1>
+      <h1 className="text-lg">What You Think</h1>
+    </div>
+  )
+}
